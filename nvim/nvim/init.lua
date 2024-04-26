2591bb45133b340b57455806f29701d4d5ce6597
Kevin W. Beam <kwbeam@fastmail.com>
2024-04-26 12:31:43 -0600

    Use lazy.nvim & mini.nvim

--- nvim/nvim/init.lua
+++ nvim/nvim/init.lua
@@ -2,8 +2,20 @@ if vim.g.vscode then
   -- VSCode extension is running, do not use plugins
   require("kwb.options")
 else
-  require("kwb.options")
-  require("kwb.bootstrap")
-  require("kwb.plugins")
-  require("kwb.config")
+  local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+  if not (vim.uv or vim.loop).fs_stat(lazypath) then
+    vim.fn.system({
+      "git",
+      "clone",
+      "--filter=blob:none",
+      "https://github.com/folke/lazy.nvim.git",
+      "--branch=stable", -- latest stable release
+      lazypath,
+    })
+  end
+  vim.opt.rtp:prepend(lazypath)
+
+  require("lazy").setup({
+    { 'echasnovski/mini.nvim', version = '*' }
+  })
 end
--- nvim/nvim/lua/kwb/README.md
+++
@@ -1,8 +0,0 @@
-## Plugins to consider
-
-  -- use 'glepnir/lspsaga.nvim'
-  -- use 'ahmedkhalf/project.nvim'
-  -- use 'akinsho/toggleterm.nvim'
-  -- use 'szw/vim-maximizer'
-  -- use 'goolord/alpha-nvim'
-  -- use 'tpope/vim-surround'
