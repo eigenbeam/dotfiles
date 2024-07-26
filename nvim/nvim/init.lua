24272059ff538ab883be2c21ef600b8378262e6f
Kevin W. Beam <kevin.beam@colorado.edu>
2024-07-25 20:51:05 -0600

    Rebasing

--- nvim/nvim/init.lua
+++ nvim/nvim/init.lua
@@ -1,9 +1,3 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-vim.g.mapleader = " "
-
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
@@ -20,37 +14,5 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-local plugins = {
-  {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {}
-  },
-  {
-    'nvim-telescope/telescope.nvim', tag = '0.1.8',
-     dependencies = { 'nvim-lua/plenary.nvim' }
-  },
-  {
-    "nvim-treesitter/nvim-treesitter", 
-    build = ":TSUpdate"
-  }
-}
-
-local opts = {}
-
-require("lazy").setup(plugins, opts)
-
-local builtin = require("telescope.builtin")
-vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-
-local configs = require("nvim-treesitter.configs")
-configs.setup({
-  ensure_installed = { "asm", "awk", "bash", "c", "clojure", "cmake", "commonlisp", "cpp", "css", "csv", "dockerfile", "forth", "fortran", "haskell", "html", "lua", "java", "javascript", "json", "make", "markdown", "matlab", "python", "racket", "query", "scheme", "terraform", "vim", "xml", "yaml" },
-  highlight = { enable = true },
-  indent = { enable = true },
-})
-
-vim.o.background = "light"
-vim.cmd.colorscheme "tokyonight"
+require("vim-options")
+require("lazy").setup("plugins")
---
+++ nvim/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,12 @@
+return {
+  "nvim-treesitter/nvim-treesitter", 
+  build = ":TSUpdate",
+  config = function()
+    local configs = require("nvim-treesitter.configs")
+    configs.setup({
+      ensure_installed = { "asm", "awk", "bash", "c", "clojure", "cmake", "commonlisp", "cpp", "css", "csv", "dockerfile", "forth", "fortran", "haskell", "html", "lua", "java", "javascript", "json", "make", "markdown", "matlab", "python", "racket", "query", "scheme", "terraform", "vim", "xml", "yaml" },
+      highlight = { enable = true },
+      indent = { enable = true },
+    })
+  end
+}
