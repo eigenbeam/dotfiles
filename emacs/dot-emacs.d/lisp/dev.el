5b392aff379194d5d5e8198e4fde9bf0c1e5e92b
Kevin W. Beam <kevin.beam@colorado.edu>
2024-07-27 08:50:40 -0600

    Remove typescript mode for now

--- emacs/dot-emacs.d/lisp/dev.el
+++ emacs/dot-emacs.d/lisp/dev.el
@@ -12,7 +12,6 @@
   :hook
   ((java-mode . lsp)
    (js-mode . lsp)
-   (typescript-ts-mode . lsp)
    (python-mode . lsp)
    (lsp-mode . lsp-enable-which-key-integration))
   :commands lsp)
@@ -47,7 +46,6 @@
   :config
   (add-hook 'js-mode-hook 'add-node-modules-path))
 
-(use-package typescript-ts-mode)
 
 ;; ----------------------------------------------------------
 ;; Java
@@ -98,25 +96,6 @@
 (defun kwb-dev-mode-hook ()
   (subword-mode))
 (add-hook 'dev-mode-hook 'kwb-dev-mode-hook)
-
-
-(setq treesit-language-source-alist
-   '((bash "https://github.com/tree-sitter/tree-sitter-bash")
-     (cmake "https://github.com/uyha/tree-sitter-cmake")
-     (css "https://github.com/tree-sitter/tree-sitter-css")
-     (elisp "https://github.com/Wilfred/tree-sitter-elisp")
-     (go "https://github.com/tree-sitter/tree-sitter-go")
-     (html "https://github.com/tree-sitter/tree-sitter-html")
-     (java "https://github.com/tree-sitter/tree-sitter-java")
-     (javascript "https://github.com/tree-sitter/tree-sitter-javascript" "master" "src")
-     (json "https://github.com/tree-sitter/tree-sitter-json")
-     (make "https://github.com/alemuller/tree-sitter-make")
-     (markdown "https://github.com/ikatyang/tree-sitter-markdown")
-     (python "https://github.com/tree-sitter/tree-sitter-python")
-     (toml "https://github.com/tree-sitter/tree-sitter-toml")
-     (tsx "https://github.com/tree-sitter/tree-sitter-typescript" "master" "tsx/src")
-     (typescript "https://github.com/tree-sitter/tree-sitter-typescript" "master" "typescript/src")
-     (yaml "https://github.com/ikatyang/tree-sitter-yaml")))
 
 
 (provide 'dev)
