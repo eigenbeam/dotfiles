75a6df3f6038ca2b39de4279003cd265035b03f1
Kevin W. Beam <kevin.beam@colorado.edu>
2024-08-13 11:33:30 -0600

    Fix nvim config formatting

--- nvim/nvim/init.lua
+++ nvim/nvim/init.lua
@@ -1,135 +1,121 @@
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
+if vim.g.vscode then
+    -- VSCode with neovim extension
+else
+    -- Neovim
+    vim.g.mapleader = " "
+    vim.g.maplocalleader = " "
+    vim.g.have_nerd_font = true
+    vim.opt.number = true
+    vim.opt.relativenumber = true
+    vim.opt.mouse = "a"
+    vim.opt.showmode = false
+    vim.opt.clipboard = "unnamedplus"
+    vim.opt.breakindent = true
+    vim.opt.undofile = true
+    vim.opt.ignorecase = true
+    vim.opt.smartcase = true
+    vim.opt.signcolumn = "yes"
+    vim.opt.updatetime = 250
+    vim.opt.timeoutlen = 300
+    vim.opt.splitright = true
+    vim.opt.splitbelow = true
+    vim.opt.list = true
+    vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+    vim.opt.inccommand = "split"
+    vim.opt.cursorline = true
+    vim.opt.scrolloff = 2
+    vim.opt.hlsearch = true
+    
+    vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+    vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous [D]iagnostic message" })
+    vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next [D]iagnostic message" })
+    vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic [E]rror messages" })
+    vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
+    vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+    vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+    vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+    vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+    vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
-vim.g.have_nerd_font = true
+    vim.api.nvim_create_autocmd("TextYankPost", {
+        desc = "Highlight when yanking (copying) text",
+        group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+        callback = function()
+            vim.highlight.on_yank()
+        end,
+    })
 
-vim.opt.number = true
-vim.opt.relativenumber = true
+    local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+    if not vim.loop.fs_stat(lazypath) then
+        local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+        vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    end ---@diagnostic disable-next-line: undefined-field
+    vim.opt.rtp:prepend(lazypath)
 
-vim.opt.mouse = "a"
+    require("lazy").setup({
+        { 'echasnovski/mini.nvim', version = '*' },
 
-vim.opt.showmode = false
+        {
+            "christoomey/vim-tmux-navigator",
+            cmd = {
+                "TmuxNavigateLeft",
+                "TmuxNavigateDown",
+                "TmuxNavigateUp",
+                "TmuxNavigateRight",
+                "TmuxNavigatePrevious",
+            },
+            keys = {
+                { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+                { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+                { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+                { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+                { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+            },
+        },
 
-vim.opt.clipboard = "unnamedplus"
+        { "numToStr/Comment.nvim", opts = {} },
 
-vim.opt.breakindent = true
+        {
+            "stevearc/conform.nvim",
+            lazy = false,
+            keys = {
+                {
+                    "<leader>f",
+                    function()
+                    	require("conform").format({ async = true, lsp_fallback = true })
+                    end,
+                    mode = "",
+                    desc = "[F]ormat buffer",
+                },
+            },
+            opts = {
+                notify_on_error = false,
+                formatters_by_ft = {
+                    lua = { "stylua" },
+                    -- Conform can also run multiple formatters sequentially
+                    -- python = { "isort", "black" },
+                    --
+                    -- You can use a sub-list to tell conform to run *until* a formatter
+                    -- is found.
+                    -- javascript = { { "prettierd", "prettier" } },
+                },
+            },
+        },
 
-vim.opt.undofile = true
+        {
+            "nvim-treesitter/nvim-treesitter",
+            build = ":TSUpdate",
+            config = function ()
+                local configs = require("nvim-treesitter.configs")
 
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-
-vim.opt.signcolumn = "yes"
-
-vim.opt.updatetime = 250
-
-vim.opt.timeoutlen = 300
-
-vim.opt.splitright = true
-vim.opt.splitbelow = true
-
-vim.opt.list = true
-vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
-
-vim.opt.inccommand = "split"
-
-vim.opt.cursorline = true
-
-vim.opt.scrolloff = 2
-
-vim.opt.hlsearch = true
-vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
-
-vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous [D]iagnostic message" })
-vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next [D]iagnostic message" })
-vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic [E]rror messages" })
-vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
-
-vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
-
-vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
-vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
-vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
-vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
-
-vim.api.nvim_create_autocmd("TextYankPost", {
-	desc = "Highlight when yanking (copying) text",
-	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
-	callback = function()
-		vim.highlight.on_yank()
-	end,
-})
-
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-	vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-end ---@diagnostic disable-next-line: undefined-field
-vim.opt.rtp:prepend(lazypath)
-
-require("lazy").setup({
-    { 'echasnovski/mini.nvim', version = '*' },
-
-	{
-		"christoomey/vim-tmux-navigator",
-		cmd = {
-			"TmuxNavigateLeft",
-			"TmuxNavigateDown",
-			"TmuxNavigateUp",
-			"TmuxNavigateRight",
-			"TmuxNavigatePrevious",
-		},
-		keys = {
-			{ "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
-			{ "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
-			{ "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
-			{ "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
-			{ "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
-		},
-	},
-
-	{ "numToStr/Comment.nvim", opts = {} },
-
-	{
-		"stevearc/conform.nvim",
-		lazy = false,
-		keys = {
-			{
-				"<leader>f",
-				function()
-					require("conform").format({ async = true, lsp_fallback = true })
-				end,
-				mode = "",
-				desc = "[F]ormat buffer",
-			},
-		},
-		opts = {
-			notify_on_error = false,
-			formatters_by_ft = {
-				lua = { "stylua" },
-				-- Conform can also run multiple formatters sequentially
-				-- python = { "isort", "black" },
-				--
-				-- You can use a sub-list to tell conform to run *until* a formatter
-				-- is found.
-				-- javascript = { { "prettierd", "prettier" } },
-			},
-		},
-	},
-
-    {
-        "nvim-treesitter/nvim-treesitter",
-        build = ":TSUpdate",
-        config = function ()
-          local configs = require("nvim-treesitter.configs")
-
-          configs.setup({
-              auto_install = true,
-			  ensure_installed = { "bash", "c", "html", "java", "javascript", "lua", "luadoc", "markdown", "python", "vim", "vimdoc" },
-              sync_install = false,
-              highlight = { enable = true },
-              indent = { enable = true },
-            })
-        end
-    },
-})
+                configs.setup({
+                    auto_install = true,
+                    ensure_installed = { "bash", "c", "html", "java", "javascript", "lua", "luadoc", "markdown", "python", "vim", "vimdoc" },
+                    sync_install = false,
+                    highlight = { enable = true },
+                    indent = { enable = true },
+                })
+            end
+        },
+    })
+end
