48be5d58dca61232fa11e429f871b9ba184da7e4
Kevin W. Beam <kevin.beam@colorado.edu>
2025-09-09 11:40:32 -0600

    git and nvim configs

--- nvim/nvim/lua/configs/lspconfig.lua
+++ nvim/nvim/lua/configs/lspconfig.lua
@@ -3,8 +3,7 @@ require("nvchad.configs.lspconfig").defaults()
 
 local lspconfig = require "lspconfig"
 
--- EXAMPLE
-local servers = { "html", "cssls", "jdtls", "jsonls", "marksman", "pyright", "ruff", "yamlls" }
+local servers = { "html", "cssls", "jdtls", "jsonls", "marksman", "pyright", "ruff", "ts_ls", "yamlls" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
